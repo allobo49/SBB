e96ad6e2935b2e4a6cd2a6933434f55dbb8758da
allobo49 <147623051+allobo49@users.noreply.github.com>
2024-05-09 19:13:53 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ import pandas as pd
 st.title('French4U 🇫🇷')
 st.write('Enter a French text below and click the button to analyze its difficulty.')
 user_input = st.text_area("Insert your text here", height=200)
-st.button('Analyze Difficulty')
+st.button('Click here to Analyze Difficulty')
