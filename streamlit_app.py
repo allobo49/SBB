9229fd3a7bac87b18486a3a87c383e0e32805a8f
allobo49 <147623051+allobo49@users.noreply.github.com>
2024-05-09 19:24:41 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import joblib 
  
 st.title('French4U 🇫🇷')
 st.write('Enter a French text below and click the button to analyze its difficulty. The difficulty is rated on a scale from 0 to 6, where 0 corresponds to a basic A1 level and 6 denotes proficiency at the C2 level.')
