2e40280c9abd05c2aad6991ed49c072787e15e3b
allobo49 <147623051+allobo49@users.noreply.github.com>
2024-05-09 19:21:00 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,17 @@ import pandas as pd
 st.title('French4U 🇫🇷')
 st.write('Enter a French text below and click the button to analyze its difficulty. The difficulty is rated on a scale from 0 to 6, where 0 corresponds to a basic A1 level and 6 denotes proficiency at the C2 level.')
 user_input = st.text_area("Insert your text here", height=200)
-st.button('Click here to Analyze Difficulty')
+
+# Load the model (make sure the model file is in the same directory or specify the full path)
+model = joblib.load('best_model_LR_features.joblib')  # Update the path as needed
+
+if st.button('Click here to Analyze Difficulty'):
+    if user_input:
+        # Assuming your model expects a DataFrame
+        data = pd.DataFrame([user_input], columns=['sentence'])
+        # Predicting the difficulty
+        prediction = model.predict(data)[0]
+        # Display the prediction
+        st.write(f'The predicted difficulty level of the text is: **{prediction}**')
+    else:
+        st.write('Please enter a text to analyze.')
